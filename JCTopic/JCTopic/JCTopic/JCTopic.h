37bc4d94ddcdf85c15055d53f1b325a0eb36411b
jeirycheng <jeiryc@126.com>
2014-01-23 18:03:03 +0800

    加入库文件

---
+++ JCTopic/JCTopic/JCTopic/JCTopic.h
@@ -0,0 +1,28 @@
+//
+//  JCTopic.h
+//  PSCollectionViewDemo
+//
+//  Created by jc on 14-1-7.
+//
+//
+
+#import <UIKit/UIKit.h>
+@protocol JCTopicDelegate<NSObject>
+-(void)didClick:(id)data;
+-(void)currentPage:(int)page;
+@end
+@interface JCTopic : UIScrollView<UIScrollViewDelegate>{
+    UIButton * pic;
+    bool flag;
+    int scrollTopicFlag;
+    NSTimer * scrollTimer;
+    int currentPage;
+    CGSize imageSize;
+    UIImage *image;
+}
+@property(nonatomic,strong)NSArray * pics;
+@property(nonatomic,retain)id<JCTopicDelegate> JCdelegate;
+@property(nonatomic,strong)UIImage * placeholderImage;
+-(void)releaseTimer;
+-(void)upDate;
+@end
---
+++ JCTopic/JCTopic/JCTopic/JCTopic.m
@@ -0,0 +1,157 @@
+//
+//  JCTopic.m
+//  PSCollectionViewDemo
+//
+//  Created by jc on 14-1-7.
+//
+//
+
+#import "JCTopic.h"
+#import "UIImageView+WebCache.h"
+@implementation JCTopic
+@synthesize JCdelegate;
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.frame = frame;
+        [self setSelf];
+    }
+    return self;
+}
+-(void)setSelf{
+    self.pagingEnabled = YES;
+    self.scrollEnabled = YES;
+    self.delegate = self;
+    self.showsHorizontalScrollIndicator = NO;
+    self.showsVerticalScrollIndicator = NO;
+    self.backgroundColor = [UIColor whiteColor];
+}
+-(UIImage*)drawImage{
+    imageSize = CGSizeMake(self.frame.size.width, self.frame.size.height);
+    UIGraphicsBeginImageContextWithOptions(imageSize, 0, [UIScreen mainScreen].scale);
+    [[UIColor blackColor] set];
+    UIRectFill(CGRectMake(0, 0, imageSize.width, imageSize.height));
+    image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    [self setSelf];
+    
+    // Drawing code
+}
+-(void)upDate{
+    NSMutableArray * tempImageArray = [[NSMutableArray alloc]init];
+    
+//    NSLog(@"%@",self.pics);
+    
+    [tempImageArray addObject:[self.pics lastObject]];
+    for (id obj in self.pics) {
+        [tempImageArray addObject:obj];
+    }
+    [tempImageArray addObject:[self.pics objectAtIndex:0]];
+    self.pics = Nil;
+    self.pics = tempImageArray;
+    //    NSLog(@"%@",self.pics);
+    
+    int i = 0;
+    for (id obj in self.pics) {
+        pic= Nil;
+        pic = [UIButton buttonWithType:UIButtonTypeCustom];
+        pic.imageView.contentMode = UIViewContentModeTop;
+        [pic setFrame:CGRectMake(i*self.frame.size.width,0, self.frame.size.width, self.frame.size.height)];
+        UIImageView * tempImage = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, pic.frame.size.width, pic.frame.size.height)];
+        tempImage.contentMode = UIViewContentModeScaleAspectFill;
+        [tempImage setClipsToBounds:YES];
+        UIImage * tempPlaceholderImage;
+        if (self.placeholderImage == Nil) {
+            tempPlaceholderImage = [self drawImage];
+        }else{
+            tempPlaceholderImage = self.placeholderImage;
+        }
+        [tempImage setImageWithURL:[NSURL URLWithString:[obj objectForKey:@"pic"]] placeholderImage:tempPlaceholderImage];
+        [pic addSubview:tempImage];
+        [pic setBackgroundColor:[UIColor grayColor]];
+        pic.tag = i;
+        [pic addTarget:self action:@selector(click:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:pic];
+        
+        UILabel * title = [[UILabel alloc]initWithFrame:CGRectMake(i*self.frame.size.width, self.frame.size.height-30, self.frame.size.width,30)];
+        [title setBackgroundColor:[UIColor blackColor]];
+        //        [title sette
+        [title setAlpha:.7f];
+        [title setText:[NSString stringWithFormat:@" %@",[obj objectForKey:@"title"]]];
+        [title setTextColor:[UIColor whiteColor]];
+        [title setFont:[UIFont fontWithName:@"Helvetica" size:12]];
+        [self addSubview:title];
+        i ++;
+    }
+    [self setContentSize:CGSizeMake(self.frame.size.width*[self.pics count], self.frame.size.height)];
+    [self setContentOffset:CGPointMake(self.frame.size.width, 0) animated:NO];
+    
+    if (scrollTimer) {
+        [scrollTimer invalidate];
+        scrollTimer = nil;
+        
+    }
+    if ([self.pics count]>3) {
+        scrollTimer = [NSTimer scheduledTimerWithTimeInterval:3.0f target:self selector:@selector(scrollTopic) userInfo:nil repeats:YES];
+    }
+}
+-(void)click:(id)sender{
+    [JCdelegate didClick:[self.pics objectAtIndex:[sender tag]]];
+}
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    
+    CGFloat Width=self.frame.size.width;
+    if (scrollView.contentOffset.x == self.frame.size.width) {
+        flag = YES;
+    }
+    if (flag) {
+        if (scrollView.contentOffset.x <= 0) {
+            [self setContentOffset:CGPointMake(Width*([self.pics count]-2), 0) animated:NO];
+        }else if (scrollView.contentOffset.x >= Width*([self.pics count]-1)) {
+            [self setContentOffset:CGPointMake(self.frame.size.width, 0) animated:NO];
+        }
+    }
+//
+    currentPage = scrollView.contentOffset.x/self.frame.size.width-1;
+    [JCdelegate currentPage:currentPage];
+    scrollTopicFlag = currentPage+2==2?2:currentPage+2;
+}
+-(void)scrollTopic{
+    [self setContentOffset:CGPointMake(self.frame.size.width*scrollTopicFlag, 0) animated:YES];
+    
+    if (scrollTopicFlag > [self.pics count]) {
+        scrollTopicFlag = 1;
+    }else {
+        scrollTopicFlag++;
+    }
+    //    NSLog(@"scrollTopicFlag %d",scrollTopicFlag);
+}
+-(void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+    scrollTimer = [NSTimer scheduledTimerWithTimeInterval:3.0f target:self selector:@selector(scrollTopic) userInfo:nil repeats:YES];
+}
+-(void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+    if (scrollTimer) {
+        [scrollTimer invalidate];
+        scrollTimer = nil;
+    }
+    
+}
+-(void)releaseTimer{
+    if (scrollTimer) {
+        [scrollTimer invalidate];
+        scrollTimer = nil;
+        
+    }
+}
+
+@end
